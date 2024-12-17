7e2e1a239122f89fc53b4a8bd8443f5cd9293184
harshivkrishna <harshivkrishnam.cse2023@citchennai.net>
2024-12-17 19:13:41 +0530

    files updated

---
+++ data/index.ts
@@ -0,0 +1,213 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly trying to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a IPC Detection Project",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Netflix - Clone",
+    des: "Developed a responsive Netflix clone with user authentication, dynamic content streaming, and an intuitive UI using modern web technologies.",
+    img: "/netflix.png",
+    iconLists: ["/re.svg", "/css.svg", "/javascript.svg", "/firebase.svg", "/api.svg"],
+    link: "https://netflix-clone-ashen-rho.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Youtube - Clone",
+    des: "Built a fully responsive YouTube clone with video playback, search functionality, and user interaction features using modern web technologies",
+    img: "/youtube.png",
+    iconLists: ["/re.svg", "/css.svg", "/javascript.svg", "/api.svg"],
+    link: "https://youtube-clone-lovat-tau.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Cafe Website",
+    des: "Designed and developed a modern, responsive cafe website showcasing menus, gallery, and contact features to enhance customer engagement and experience.",
+    img: "/project1.png",
+    iconLists: ["/html.svg", "/css.svg", "/javascript.svg"],
+    link: "https://harshivkrishna.github.io/Coffee-shop-website/",
+  },
+  {
+    id: 4,
+    title: "Ornamental Plant Website",
+    des: "Created a visually appealing and responsive ornamental plant website featuring product catalogs, care guides, and seamless shopping integration for plant enthusiasts.",
+    img: "/project2.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://fancy-scone-e85c28.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Web Development Intern - GAO Tek. Inc",
+    desc: "Assisted in the development of a websites using WordPress and Elementor, enhancing web optimization.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Full Stack Web Developer Intern - Znyck Technologies Pvt Ltd",
+    desc: "Developed websites with lastest Techs using ReactJS, NextJS ",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Web Developer",
+    desc: "Led the development of website for a client, from initial concept to deployment on web.",
+    className: "md:col-span-2", 
+    thumbnail: "/exp3.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link:"https://github.com/harshivkrishna"
+  },
+  {
+    id: 2,
+    img: "/insta.svg",
+    link:"https://www.instagram.com/harshivkrishna/"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link:"www.linkedin.com/in/harshivkrishnam"
+  },
+];
